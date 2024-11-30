c38c9e222ab0b81d50037641999afb655649c2f9
NithinThota79 <nithinthota630@gmail.com>
2024-11-30 21:53:33 +0530

    Created board with random colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-function App() {
+import { useEffect, useState } from "react";
+
+const width = 8;
+const candyColors = [
+   'blue',
+   'green',
+   'orange',
+   'purple',
+   'red',
+   'yellow',
+    
+]
+
+const App = () => {
+  const [currentColorArragement, setCurrentColorArragement] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for(let i=0; i<width*width; i++){
+      const randomColor = candyColors[Math.floor(Math.random()*candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArragement(randomColorArrangement);    
+  }
+
+  useEffect( () => {
+    createBoard()
+  }, [])
+
+  console.log(currentColorArragement);
+
   return (
-    <div className="App">
-      <h1>Our Project Starts from Here</h1>
+    <div className="app">
+      <div className = "game">
+        {currentColorArragement.map((candyColor, index) =>(
+          <img 
+            key={index}
+            style={{backgroundColor: candyColor}}
+          />
+        ))}
+      </div>
     </div>
   );
 }
